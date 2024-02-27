e0b976cdd4d8a593421235c464505328615b4478
JPLDN <jujumaster2603@gmail.com>
2024-02-27 14:12:29 +0000

    Updated FPS Counter

--- Assets/Script/FPSCounter.cs
+++ Assets/Script/FPSCounter.cs
@@ -5,6 +5,7 @@ using TMPro;
 public class FPSCounter : MonoBehaviour
 {
     public TextMeshProUGUI FpsText;
+    public TextMeshProUGUI msPerFrameText;
 
     public float pollingTime = 3f;
     private float time;
@@ -20,7 +21,13 @@ public class FPSCounter : MonoBehaviour
         if (time >= pollingTime)
         {
             int frameRate = Mathf.RoundToInt(frameCount / time);
+            float milisecondsPerFrame = 1000.0f / frameRate;
+
+            // Display FPS
             FpsText.text = frameRate.ToString() + " FPS";
+
+            // Display miliseconds per frame
+            msPerFrameText.text = milisecondsPerFrame.ToString("F2") + " ms/frame";
 
             time -= pollingTime;
             frameCount = 0;
